71ba84820ef1877edefb6c994f179cee6ac852dd
Dougal Graham <dougalg@gmail.com>
2013-11-13 22:59:21 +0700

    Fleshing out the first version of the plugin

--- change_money.js
+++ change_money.js
@@ -2,9 +2,33 @@ function get_currency_list(currency_string) {
     return currency_string.split(/\s*,\s*/);
 }
 
-function make_link(text, amount, id) {
-    var linkSpan = $(document.createElement("span")).click(showHideConversion);
-    var link = $(document.createElement("a")).click(showHideConversion);
+function make_link(link_text, amount, id) {
+    var link_span = $(document.createElement("span"));
+    var link = $(document.createElement("a")).html(link_text).click(function(){showHideConversion(id);});
+    link_span.append(link.before('[ ').after(' ]'));
+    return link_span;
+
+}
+
+function make_table(amount, from, to, id) {
+    var new_table = $(document.createElement("table"));
+    var header_row = $(document.createElement("tr"));
+    var data_row = $(document.createElement("tr"));
+
+    var num_conversions = to.length,
+        new_el,
+        i;
+    for (i=0;i<num_conversions;i++) {
+        new_el = $(document.createElement("th")).html(to[i]);
+        header_row.append(new_el);
+    }
+    for (i=0;i<num_conversions;i++) {
+        new_el = $(document.createElement("td")).html(currency_convert(amount, from, to[i]));
+        data_row.append(new_el);
+    }
+    new_table.append(header_row).append(data_row);
+
+    return new_table;
 }
 
 function showHideConversion() {
@@ -12,10 +36,8 @@ function showHideConversion() {
     return false;
 }
 
-function convert_money(amount, from, to, round, id, text) {
-    var current_id = get_id(id);
+function convert_money(amount, from, to, round, id, link_text) {
     var to_currencies = get_currency_list(to);
     var container = $('#'+id);
-
-    make_link(text, amount, id);
+    container.html('asd');
 }
--- money-changer.php
+++ money-changer.php
@@ -14,8 +14,9 @@ $fn_pref = "money_changer_";
 $DEFAULT_LINK_TEXT = "Other Currencies";
 
 function enqueue_coin_scripts() {
+    global $plugin_url;
     wp_register_script( 'coinmill', 'http://coinmill.com/frame.js');
-    wp_register_script( 'coinmill-custom', $plugin_url.'change_money.js');
+    wp_register_script( 'coinmill-custom', $plugin_url.'change_money.js', array( 'jquery' ));
     
     wp_enqueue_script( 'coinmill' );
     wp_enqueue_script( 'coinmill-custom' );
@@ -23,12 +24,14 @@ function enqueue_coin_scripts() {
 add_action('wp_enqueue_scripts', 'enqueue_coin_scripts');
 
 function get_id($id) {
+    global $fn_pref;
     if ($id === null || $id === '')
         $id = uniqid();
-    return $fn_pref+$id;
+    return $fn_pref.$id;
 }
 
 function change_money($atts, $link_content = null) {
+    global $DEFAULT_LINK_TEXT;
     extract(shortcode_atts(array(
         "amount" => 0,
         "from" => 'USD',
@@ -37,9 +40,10 @@ function change_money($atts, $link_content = null) {
         "id" => null
     ), $atts));
     $id = get_id($id);
-    if ($link_content === null)
+    if ($link_content === null || $link_content === '')
         $link_content = $DEFAULT_LINK_TEXT;
-    return "<script type=\"text/javascript\">/*<![CDATA[*//*---->*/convert_money($amount, \"$from\", \"$to\", $round, $id, $link_content)/*--*//*]]>*/</script><div id='$id'></div>";
+    return "<script type=\"text/javascript\">/*<![CDATA[*//*---->*/convert_money($amount, \"$from\", \"$to\", $round, \"$id\", \"$link_content\")/*--*//*]]>*/</script><div id='$id'></div>";
 }
 add_shortcode("change_money", "change_money");
+
 ?>
